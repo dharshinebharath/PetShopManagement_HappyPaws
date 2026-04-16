8171352d5aace43bf1f9c3ddfb3d65d8571c589c
shirlly <shirllyshirlly20@gmail.com>
2026-04-16 11:09:49 +0530

    updated

---
+++ pet_shop/src/main/java/com/sprint/pet_shop/service/GroomingServicesService.java
@@ -0,0 +1,57 @@
+package com.sprint.pet_shop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.sprint.pet_shop.entity.GroomingServices;
+import com.sprint.pet_shop.repository.GroomingServicesRepository;
+import com.sprint.pet_shop.service.interfaces.GroomingServicesInterface;
+
+import jakarta.validation.Valid;
+
+@Service
+public class GroomingServicesService implements GroomingServicesInterface {
+
+	@Autowired
+	private GroomingServicesRepository groomingServicesRepository;
+	
+	public List<GroomingServices> saveAllGroomingServices(List<GroomingServices> groomingServices)
+	{
+		return groomingServicesRepository.saveAll(groomingServices);
+	}
+
+	@Override
+	public List<GroomingServices> getAllGroomingServices() {
+		return groomingServicesRepository.findAll();
+	}
+
+	@Override
+	public GroomingServices getGroomingServiceById(long id) {
+		return groomingServicesRepository.findById(id).orElseThrow(()->new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Grooming Service Not Found")); 
+	}
+	
+	@Override
+	public void deleteGroomingServiceById(long id)
+	{
+		GroomingServices existing=groomingServicesRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Grooming Services Not Found"));
+		groomingServicesRepository.delete(existing);
+	}
+
+	@Override
+	public GroomingServices updateGroomingService(long id, @Valid GroomingServices service) {
+		GroomingServices existing = groomingServicesRepository.findById(id)
+	            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Grooming Services Not Found"));
+
+	    existing.setName(service.getName());
+	    existing.setDescription(service.getDescription());
+	    existing.setPrice(service.getPrice());
+	    existing.setAvailable(service.getAvailable());
+
+	    return groomingServicesRepository.save(existing);
+	}
+}
