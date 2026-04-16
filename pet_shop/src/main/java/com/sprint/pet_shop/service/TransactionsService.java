2b4d9100b204964dcdb2849ff3ac5fafd4cb248f
priyaaa <priyadharshinirss356@gmail.com>
2026-04-16 17:42:39 +0530

    Priya (#7)
    * done
    
    * commit
    
    * updated
    
    ---------
    
    Co-authored-by: swif13-t <swiftie@gmail.com>

--- pet_shop/src/main/java/com/sprint/pet_shop/service/TransactionsService.java
+++ pet_shop/src/main/java/com/sprint/pet_shop/service/TransactionsService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sprint.pet_shop.entity.TransactionsEntity;
+import com.sprint.pet_shop.exception.InvalidDataException;
+import com.sprint.pet_shop.exception.ResourceNotFoundException;
 import com.sprint.pet_shop.repository.TransactionsRepository;
 import com.sprint.pet_shop.service.interfaces.TransactionsInterface;
 
@@ -13,10 +15,17 @@ import com.sprint.pet_shop.service.interfaces.TransactionsInterface;
 public class TransactionsService implements TransactionsInterface{
 	@Autowired 
 	private TransactionsRepository transactionsRepository;
+	//save
 	@Override
-	public TransactionsEntity saveTransactionsEntity(TransactionsEntity transactionsEntity) {
-		return transactionsRepository.save(transactionsEntity);
+	public TransactionsEntity saveTransactionsEntity(TransactionsEntity entity) {
+
+	    if (entity.getCustomerId() == null || entity.getPetId() == null) {
+	        throw new InvalidDataException("Customer ID and Pet ID cannot be null");
+	    }
+
+	    return transactionsRepository.save(entity);
 	}
+	//saveall
 	@Override
 	public List<TransactionsEntity> TransactionsEntityAll(List<TransactionsEntity> transactionsEntity){
 		return transactionsRepository.saveAll(transactionsEntity);
@@ -30,36 +39,41 @@ public class TransactionsService implements TransactionsInterface{
     //get by id
 	@Override
     public TransactionsEntity getTransactionById(Long id) {
-        return transactionsRepository.findById(id).orElse(null);
+		return transactionsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Transaction not found with id: " + id));
     }
 	@Override
     public TransactionsEntity updateTransaction(Long id, TransactionsEntity newData) {
 
         TransactionsEntity existing = transactionsRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Transaction not found with id: " + id));
+        		.orElseThrow(() -> new ResourceNotFoundException("Transaction not found with id: " + id));
+        
+        if (newData.getCustomerId() == null || newData.getPetId() == null) {
+            throw new InvalidDataException("Customer ID and Pet ID cannot be null");
+        }
+
 
         existing.setCustomerId(newData.getCustomerId());
         existing.setPetId(newData.getPetId());
         existing.setTransactionDate(newData.getTransactionDate());
         existing.setAmount(newData.getAmount());
         existing.setTransactionStatus(newData.getTransactionStatus());
+        
 
         return transactionsRepository.save(existing);
     }
 
     // DELETE
 	@Override
-    public String deleteTransaction(Long id) {
-        if (transactionsRepository.existsById(id)) {
-            transactionsRepository.deleteById(id);
-            return "Deleted successfully";
-        }
-        return "Transaction not found";
-    }
+	public String deleteTransaction(Long id) {
+
+	    if (!transactionsRepository.existsById(id)) {
+	        throw new ResourceNotFoundException("Transaction not found with id: " + id);
+	    }
+
+	    transactionsRepository.deleteById(id);
+	    return "Deleted successfully";
+	}
     
-
-
-   
 }
 
 
