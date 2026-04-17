581013357db7bb999c7bf697880420336e4b7174
dharshinebharath <157278216+dharshinebharath@users.noreply.github.com>
2026-04-17 13:22:27 +0530

    Slave5 (#11)
    * updated
    
    * mapping done
    
    ---------
    
    Co-authored-by: Revathikandan <157710486+Revathikandan@users.noreply.github.com>

--- pet_shop/src/main/java/com/sprint/pet_shop/controller/GroomingServicesController.java
+++ pet_shop/src/main/java/com/sprint/pet_shop/controller/GroomingServicesController.java
@@ -3,6 +3,8 @@ package com.sprint.pet_shop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sprint.pet_shop.dto.requestDto.GroomingServicesRequestDTO;
+import com.sprint.pet_shop.dto.responseDto.ApiResponse;
+import com.sprint.pet_shop.dto.responseDto.GroomingServicesResponseDTO;
 import com.sprint.pet_shop.entity.GroomingServices;
 import com.sprint.pet_shop.service.GroomingServicesService;
 
@@ -26,35 +31,48 @@ public class GroomingServicesController {
 	private GroomingServicesService groomingServicesService;
 	
 	@PostMapping
-	public List<GroomingServices> saveAllGroomingServices(@Valid @RequestBody List<GroomingServices> groomingServices)
-	{
-		return groomingServicesService.saveAllGroomingServices(groomingServices);
-	}
+	 public ResponseEntity<ApiResponse<List<GroomingServicesResponseDTO>>> saveAll(
+	            @Valid @RequestBody List<GroomingServicesRequestDTO> dtos) {
+
+	        ApiResponse<List<GroomingServicesResponseDTO>> response =
+	                groomingServicesService.saveAllGroomingServices(dtos);
+
+	        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	    }
 	
 	@GetMapping
-	public List<GroomingServices> getAllGroomingServices()
-	{
-		return groomingServicesService.getAllGroomingServices();
-	}
+	 public ResponseEntity<ApiResponse<List<GroomingServicesResponseDTO>>> getAll() {
+
+        ApiResponse<List<GroomingServicesResponseDTO>> response =
+                groomingServicesService.getAllGroomingServices();
+
+        return ResponseEntity.ok(response);
+    }
+	@GetMapping("/{id}")
+	   public ResponseEntity<ApiResponse<GroomingServicesResponseDTO>> getById(
+	            @PathVariable long id) {
+
+	        ApiResponse<GroomingServicesResponseDTO> response =
+	                groomingServicesService.getGroomingServiceById(id);
+
+	        return ResponseEntity.ok(response);
+	    }
 	
-	@GetMapping("/{serviceId}")
-	public GroomingServices getGroomingServicesById(@PathVariable long serviceId)
-	{
-		return groomingServicesService.getGroomingServiceById(serviceId);
-	}
-	
-	@DeleteMapping("/{serviceId}")
+	@DeleteMapping("/{id}")
 	public String deleteGroomingServiceById(@PathVariable long serviceId)
 	{
 		groomingServicesService.deleteGroomingServiceById(serviceId);
 		return "Vaccination Deleted Successfully";
 	}
 	
-	@PutMapping("/{serviceId}")
-	public GroomingServices updateGroomingService(
-	        @PathVariable long serviceId,
-	        @Valid @RequestBody GroomingServices service) {
+	@PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<GroomingServicesResponseDTO>> update(
+            @PathVariable long id,
+            @Valid @RequestBody GroomingServicesRequestDTO dto) {
 
-	    return groomingServicesService.updateGroomingService(serviceId, service);
-	}
+        ApiResponse<GroomingServicesResponseDTO> response =
+                groomingServicesService.updateGroomingService(id, dto);
+
+        return ResponseEntity.ok(response);
+    }
 }
