44b7638fcca7291c16e5175e66bfea1f0b3c1601
shirlly <shirllyamalarass@gmail.com>
2026-04-20 13:36:36 +0530

    proj

--- frontend/src/app/features/grooming/grooming-list/grooming-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-grooming-list',
-  imports: [],
-  templateUrl: './grooming-list.html',
-  styleUrl: './grooming-list.css',
-})
-export class GroomingList {}
