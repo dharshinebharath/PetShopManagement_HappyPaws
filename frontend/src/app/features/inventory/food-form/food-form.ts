f71f88d79cd47a3b4e4ad36a3907f21898430fe0
dharshine <dharshinebharath@gmail.com>
2026-04-20 09:18:37 +0530

    frontend added

---
+++ frontend/src/app/features/inventory/food-form/food-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-form',
+  imports: [],
+  templateUrl: './food-form.html',
+  styleUrl: './food-form.css',
+})
+export class FoodForm {}
