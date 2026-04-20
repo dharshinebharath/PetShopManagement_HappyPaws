9bdbcd7aa3960fdeeea7c45e23aa8afa30582392
Revathi <revathi@gmail.com>
2026-04-20 13:38:15 +0530

    updated

--- frontend/src/app/features/vaccinations/vaccination-list/vaccination-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-vaccination-list',
-  imports: [],
-  templateUrl: './vaccination-list.html',
-  styleUrl: './vaccination-list.css',
-})
-export class VaccinationList {}
