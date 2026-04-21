f819c2bdf42e01f2842cd5901378d39709a53002
shirlly <shirllyamalarass@gmail.com>
2026-04-21 07:22:18 +0530

    proj

--- frontend/src/app/features/services/grooming-form/grooming-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-grooming-form',
-  imports: [],
-  templateUrl: './grooming-form.html',
-  styleUrl: './grooming-form.css',
-})
-export class GroomingForm {}
